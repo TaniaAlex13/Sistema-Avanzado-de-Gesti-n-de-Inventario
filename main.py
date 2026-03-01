1b7d03c9c94193778a47e563084be474e5454062
TaniaAlex13 <ta.toapantac@uea.edu.ec>
2026-03-01 12:28:27 -0500

    Paso 1: Estructura inicial del proyecto

---
+++ main.py
@@ -0,0 +1,11 @@
+# Sistema Avanzado de Gestión de Inventario
+# Autor: Tania Alex
+# Descripción: Sistema que gestiona productos utilizando POO,
+# colecciones y almacenamiento en archivos.
+
+def main():
+    print("Sistema de Inventario iniciado correctamente.")
+
+if __name__ == "__main__":
+    main()
+    
